6229890c7870490055b00c187837abf8f7b5dcdc
Marivaldo Sena <marivaldo.sena@live.com>
2020-09-14 09:11:03 -0300

    Initial Commit

---
+++ VoadorProtocol/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  VoadorProtocol
+//
+//  Created by Marivaldo Sena on 14/09/20.
+//  Copyright © 2020 Marivaldo Sena. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
